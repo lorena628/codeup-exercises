96552d4a1f71833d9526e07cbb48f873cdb96ff7
lo v <lov@los-MacBook-Pro.local>
2016-04-15 09:55:42 -0500

    ran first line of php

---
+++ hello_world.php
@@ -0,0 +1,13 @@
+<?php 
+$firstName = "lorena";
+$lastName= "villanueva";
+
+$name = $firstName . " " .$lastName;
+echo "hello my name is ". $name . PHP_EOL;
+
+$name = "$firstName $lastName\n";
+echo "hello my name is ". $name;
+
+$name = '$firstName $lastName\n';
+echo "hello my name is ". $name;
+
