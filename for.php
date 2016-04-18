f86525ae717ca19017d510783fe100e69ca5274c
lo v <lov@los-MacBook-Pro.local>
2016-04-18 14:04:51 -0500

    added defaults to the for loop if no user input

--- for.php
+++ for.php
@@ -1,19 +1,30 @@
 <?php
 
-    echo "Pick a starting number\n";   
-    $userStart = trim(fgets(STDIN));
-    
-    if(!is_numeric($userStart)){
-        echo "this is not a number\n";
-    exit;
-    }
-    echo"Pick an ending number\n";
-    $userEnd = trim(fgets(STDIN));
-    if(!is_numeric($userEnd)){
-        echo "this is not a number\n";
-        exit;
-    }  
-
-for ($a = $userStart; $a <= $userEnd; $a++){
+echo "Pick a starting number\n";   
+//user start input
+$userStart = trim(fgets(STDIN));
+if(!is_numeric($userStart)){
+    echo "this is not a number\n";
+    //default start
+    $userStart = 2;
+}
+//user end input
+echo"Pick an ending number\n";
+$userEnd = trim(fgets(STDIN));
+if(!is_numeric($userEnd)){
+    echo "this is not a number\n";
+    //default end
+    $userEnd = 100;
+} 
+//increment count
+echo "Count by increments of what? \n";
+$userCount = trim(fgets(STDIN));
+if(!is_numeric($userCount)){
+    echo "this is not a number\n";
+    //default increment
+    $userCount=1;
+}   
+//for loop
+for ($a = $userStart; $a <= $userEnd; $a+=$userCount){
     echo "Your number is {$a}\n";
 }
