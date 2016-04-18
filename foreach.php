9c00f3724185ecbbf0b8a0016cb0352fbf038249
lo v <lov@los-MacBook-Pro.local>
2016-04-18 15:18:57 -0500

    foreach loop for will output each type of value in our example

---
+++ foreach.php
@@ -0,0 +1,30 @@
+<?php
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+foreach ($things as $valType) {
+    if(is_string($valType)){
+        echo "{$valType} \n";
+    }elseif (is_integer($valType)){
+        echo "{$valType} ";
+    }elseif (is_null($valType)){
+        echo "{$valType}\n";
+    }elseif(is_array($valType)) {
+        //cannot print out nested array so need a new foreach to print out what's in the array
+       foreach ($valType as $arrayItem) {
+       echo "{$arrayItem} \n";    
+       }
+    }elseif(is_numeric($valType)) {
+        echo "{$valType} \n";
+    }elseif(is_bool($valType)) {
+        echo "{$valType} \n";
+    }
+}
+
+
+//this will show everything that is not an array or null
+// foreach ($things as $valType) {
+//     if(is_scalar($valType)){
+//         echo "{$valType} \n";
+//     }
+// }
