5b6f807ca00416469cb7ec24a3f9e7396aef6383
lo v <lov@los-MacBook-Pro.local>
2016-04-19 11:48:34 -0500

    used switchcase for day of week

---
+++ switch.php
@@ -0,0 +1,51 @@
+<?php
+
+ // Set the default timezone
+ date_default_timezone_set('America/Chicago');
+
+ // Get Day of Week as number
+ // 1 (for Monday) through 7 (for Sunday)
+ $dayOfWeek = date('N');
+
+//this is break case
+ switch($dayOfWeek) {
+    case 1:
+        echo "It's Monday \n";
+        break;
+    case 2:
+        echo "It's Tuesday \n";
+        break;
+    case 3:
+        echo "It's Wednesday \n";
+        break;
+    case 4:
+        echo "It's Thursday \n";
+        break;
+    case 5: 
+        echo "It's friday \n";
+        break;
+    case 6: 
+        echo "It's saturday \n";
+        break;
+    case 7: 
+        echo "It's sunday \n";
+        break;
+ }
+
+ // $dayOfWeek = date('N');
+
+ if($dayOfWeek==1){
+    echo "It's monday \n";
+ }elseif($dayOfWeek==2){
+    echo "It's tuesday \n";
+ }elseif($dayOfWeek==3){
+    echo "It's Wednesday \n";
+ }elseif($dayOfWeek==4){
+    echo "It's thursday \n";
+ }elseif($dayOfWeek==5){
+    echo "It's friday \n";
+ }elseif($dayOfWeek==6){
+    echo "It's saturday \n";
+ }elseif($dayOfWeek==7){
+    echo "It's sunday \n";
+ }
