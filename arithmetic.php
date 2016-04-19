c0ff9aa57531cd9a4a19d6a22e313c3b9b6227ab
lo v <lov@los-MacBook-Pro.local>
2016-04-19 14:27:48 -0500

    added a modulus function

--- arithmetic.php
+++ arithmetic.php
@@ -19,9 +19,14 @@ function divide($a, $b)
 {
     return $a / $b;
 }
+function modulus($a, $b)
+{
+    return $a % $b;
+}
 
 // Add code to test your functions here
 echo add(5, 5) . PHP_EOL;
 echo subtract(10 ,5) . PHP_EOL;
 echo multiply(5, 20) . PHP_EOL;
 echo divide(100, 20) . PHP_EOL;
+echo modulus(20, 3) . PHP_EOL;
