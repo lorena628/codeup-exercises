dc734d38e705cb1c647f9de01b35a6a6dac8e1c8
lo v <lov@los-MacBook-Pro.local>
2016-04-19 11:06:29 -0500

    used break in a for loop

--- break_continue.php
+++ break_continue.php
@@ -1,12 +1,20 @@
 <?php
 
 
-//this first for loop prints out all even
+//this first for loop prints out all even using continue
+// for($i=1;$i <=100; $i++){
+//     echo "{$i}\n";
+//     if($i%2!=0){
+    
+//     continue;
+//     }
+//         echo "^this is even \n";
+// }
+
+//this second for loop uses break to stop at 10
 for($i=1;$i <=100; $i++){
     echo "{$i}\n";
-    if($i%2!=0){
-    
-    continue;
-    }
-        echo "^this is even \n";
+    if ($i == 10) {
+        break;
+    }    
 }
