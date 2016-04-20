28825ea9452883a6e7e3b905a47bece314260fd0
lo v <lov@los-MacBook-Pro.local>
2016-04-19 21:29:36 -0500

    added a throwErrorMessage function that will outbut errors for all arithmetic functions when called

--- arithmetic.php
+++ arithmetic.php
@@ -10,7 +10,7 @@ function add($a, $b)
     if(is_numeric($a) && is_numeric($b)){
         return $a + $b;    
     }else {
-        return "ERROR: Arguments must be numeric, you entered {$a} and {$b}\n";
+        return throwErrorMessage($a, $b);
     }
 }
 function subtract($a, $b)
@@ -18,7 +18,7 @@ function subtract($a, $b)
     if(is_numeric($a) && is_numeric($b)){ 
         return $a - $b;
     }else {
-        return "ERROR: Arguments must be numeric, you entered {$a} and {$b}\n";
+        return throwErrorMessage($a, $b);
     }
 }
 function multiply($a, $b)
@@ -26,7 +26,7 @@ function multiply($a, $b)
     if(is_numeric($a) && is_numeric($b)){
         return $a * $b;
     }else {
-        return "ERROR: Arguments must be numeric, you entered {$a} and {$b}\n";
+        return throwErrorMessage($a, $b);
     }
 }
 function divide($a, $b)
@@ -37,7 +37,7 @@ function divide($a, $b)
         }
         return $a / $b;
     }else {
-         return "ERROR: Must be numeric, you entered {$a} and {$b}";
+        return throwErrorMessage($a, $b);
     }
 }
 function modulus($a, $b)
@@ -45,12 +45,16 @@ function modulus($a, $b)
     if(is_numeric($a) && is_numeric($b)){
         return $a % $b;
     }else {
-        return "ERROR: Arguments must be numeric, you entered {$a} and {$b}\n";
+        return throwErrorMessage($a, $b);
     }   
 }
+function throwErrorMessage($a, $b)
+{
+    return "ERROR: Arguments must be numeric, you entered {$a} and {$b}";
+}
 // Add code to test your functions here
-echo add('tdf', 5) . PHP_EOL;
+echo add(5, 5) . PHP_EOL;
 echo subtract(10 , 5) . PHP_EOL;
 echo multiply(5, 20) . PHP_EOL;
-echo divide(90, 9) . PHP_EOL;
+echo divide(90, 0) . PHP_EOL;
 echo modulus(20, 3) . PHP_EOL;
