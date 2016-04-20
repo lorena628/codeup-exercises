2e1a245bda64d6f4744c32230d860ef1c76a12b6
lo v <lov@los-MacBook-Pro.local>
2016-04-19 21:05:37 -0500

    adjustments made to division function to output error if divisible by 0

--- arithmetic.php
+++ arithmetic.php
@@ -1,32 +1,56 @@
 <?php
 
+//these variables cannot be called from inside the function, they are
+//global but if you move inside the function they will be modified
+
+// $a = 6;
+// $b = 3;
 function add($a, $b)
 {
-    return $a + $b;
+    if(is_numeric($a) && is_numeric($b)){
+        return $a + $b;    
+    }else {
+        return "ERROR: Arguments must be numeric, you entered {$a} and {$b}\n";
+    }
 }
-
 function subtract($a, $b)
 {
-   return $a - $b;
+    if(is_numeric($a) && is_numeric($b)){ 
+        return $a - $b;
+    }else {
+        return "ERROR: Arguments must be numeric, you entered {$a} and {$b}\n";
+    }
 }
-
 function multiply($a, $b)
-{
-    return $a * $b;
+{   
+    if(is_numeric($a) && is_numeric($b)){
+        return $a * $b;
+    }else {
+        return "ERROR: Arguments must be numeric, you entered {$a} and {$b}\n";
+    }
 }
-
 function divide($a, $b)
 {
-    return $a / $b;
+    if(is_numeric($a) && is_numeric($b)){
+        if($b==0){
+            return "ERROR: Cannot divide by zero";
+        }
+        return $a / $b;
+    }else {
+         return "ERROR: Must be numeric, you entered {$a} and {$b}";
+    }
 }
 function modulus($a, $b)
-{
-    return $a % $b;
+{   
+    if(is_numeric($a) && is_numeric($b)){
+        return $a % $b;
+    }else {
+        return "ERROR: Arguments must be numeric, you entered {$a} and {$b}\n";
+    }   
 }
-
 // Add code to test your functions here
-echo add(5, 5) . PHP_EOL;
-echo subtract(10 ,5) . PHP_EOL;
+echo add('tdf', 5) . PHP_EOL;
+echo subtract(10 , 5) . PHP_EOL;
 echo multiply(5, 20) . PHP_EOL;
-echo divide(100, 20) . PHP_EOL;
+echo divide(90, 9) . PHP_EOL;
 echo modulus(20, 3) . PHP_EOL;
