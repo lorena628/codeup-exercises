58407dabd2b5a810c383b1e32232146b5c3f81e6
lo v <lov@los-MacBook-Pro.local>
2016-04-21 14:54:34 -0500

    modifications to have function defualted to not zero and will asort if a second parameter of true is passed

--- list_with_commas.php
+++ list_with_commas.php
@@ -3,18 +3,25 @@
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 $physicistsArray = explode (', ', $physicistsString);
 //making this function more general so it can apply anywhere
-function humanizedList($array)
+//the second parameter $asort=false is setting a default of not sorting 
+function humanizedList($array, $asort=false)
 {
-    asort($array);
-    $lastItem = array_pop($array);
-    $nameString = implode(', ', $array);
-    $readableList = $nameString . ', ' . ' and ' . $lastItem;
-    return $readableList;
+    if($asort!=false){
+        asort($array); 
+        $lastItem = array_pop($array);
+        $nameString = implode(', ', $array);
+        $readableList = $nameString . ', ' . ' and ' . $lastItem;
+        return $readableList;
+    }else{
+        $lastItem = array_pop($array);
+        $nameString = implode(', ', $array);
+        $readableList = $nameString . ', ' . ' and ' . $lastItem;
+        return $readableList;
+    }
 }
 //this is resetting a variable of $readable list to call function human...and the array prior to function will be passed in the parameters
-$readableList = humanizedList($physicistsArray);
+// also if you dont put ($physicistsArray, true) and leave as ($physicistsArray); it will leave the default which is unsorted
+$readableList = humanizedList($physicistsArray, true);
 echo "Some of the most famous physicists are $readableList." . PHP_EOL;
-//var_dump($readableList);
-// $fakePhysicists = humanizedList($physicistsArray);
-// echo "Some of the most famous fake physicists are $fakePhysicists";
+
 
