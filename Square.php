e9f9f5bed626da5b5a2a4537de15ad100673ba2b
lo v <lov@los-MacBook-Pro.local>
2016-05-17 10:25:43 -0500

    modified the method for perimeter

--- Square.php
+++ Square.php
@@ -3,6 +3,7 @@ class Square extends Rectangle
 {
     public function perimeter() 
     {
-        return ($this->width * 2) + ($this->height * 2) ;
+        return $this->width * 4;
+        // return ($this->width * 2) + ($this->height * 2) ;
     }
 };
