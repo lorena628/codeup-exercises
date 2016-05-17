86e9fa76bb902faa90b05f7179cdaac8c87ace05
lo v <lov@los-MacBook-Pro.local>
2016-05-17 10:26:34 -0500

    modified the heights and widths of tester shapes

--- shapes_test.php
+++ shapes_test.php
@@ -3,15 +3,16 @@ require_once 'Rectangle.php';
 require_once 'Square.php';
 
 $rectangle = new Rectangle(10, 3);
-echo $rectangle ->area() . PHP_EOL;
+echo "area of a rectangle with w of 10 and h of 3 is " . $rectangle->area() . PHP_EOL;
 
+$rectangle = new Rectangle(10, 0);
+echo "area of a rectangle with w of 10 and h of 0 is " . $rectangle->area() . PHP_EOL;
+ 
 
-$square = new Square(2 , 2);
-echo "perimeter of a square with side of 2 is " . $square -> perimeter() . PHP_EOL;
+$square = new Square(2, 0);
+echo "perimeter of a square with side of 2 is " . $square->perimeter() . PHP_EOL;
 
 
 $square = new Square(3 , 2);
-echo  $square -> area() . PHP_EOL;
-
-$square = new Square(3 , 2);
-echo  $square -> perimeter() . PHP_EOL;
+echo "\"area\" of a square with w of 3 and h of 2 is " . $square->area() . PHP_EOL;
+echo "the perimeter of square is " . $square->perimeter() . PHP_EOL;
