c53f9f484fc8032ac6b485fe36722443eef3c21a
lo v <lov@los-MacBook-Pro.local>
2016-05-17 11:36:28 -0500

    modifications to test overridding and base methods

--- shapes_test.php
+++ shapes_test.php
@@ -2,17 +2,30 @@
 require_once 'Rectangle.php';
 require_once 'Square.php';
 
-$rectangle = new Rectangle(10, 3);
-echo "area of a rectangle with w of 10 and h of 3 is " . $rectangle->area() . PHP_EOL;
+// $rectangle = new Rectangle(10, 3);
+// echo "area of a rectangle with w of 10 and h of 3 is " . $rectangle->area() . PHP_EOL;
 
-$rectangle = new Rectangle(10, 0);
-echo "area of a rectangle with w of 10 and h of 0 is " . $rectangle->area() . PHP_EOL;
+// $rectangle = new Rectangle(10, 0);
+// echo "area of a rectangle with w of 10 and h of 0 is " . $rectangle->area() . PHP_EOL;
  
 
+// $square = new Square(2, 0);
+// echo "perimeter of a square with side of 2 is " . $square->perimeter() . PHP_EOL;
+
+
+// $square = new Square(3 , 2);
+// echo "\"area\" of a square with w of 3 and h of 2 is " . $square->area() . PHP_EOL;
+// echo "the perimeter of square is " . $square->perimeter() . PHP_EOL;
+
+
+$rectangle = new Rectangle(10, 3);
+echo "area of a rectangle 10 X 3 = " . $rectangle->area() . PHP_EOL;
+echo "perimeter of a rectangle 10 X 3 = " . $rectangle->perimeter() . PHP_EOL;
+
+
+
 $square = new Square(2, 0);
-echo "perimeter of a square with side of 2 is " . $square->perimeter() . PHP_EOL;
+echo "perimeter of a square with side of 2 is = " . $square->perimeter() . PHP_EOL;
+echo "area of a square with side of 2 is = " . $square->area() . PHP_EOL;
 
 
-$square = new Square(3 , 2);
-echo "\"area\" of a square with w of 3 and h of 2 is " . $square->area() . PHP_EOL;
-echo "the perimeter of square is " . $square->perimeter() . PHP_EOL;
